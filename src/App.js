13717baf54dbb76b16fd566522bf82a33937d8c1
Rajarshi Chattopadhyay <likarajo@gmail.com>
2021-01-21 10:58:32 -0600

    added code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 import './App.css';
 
 function App() {
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [result, setResult] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch('https://api.github.com/users/likarajo/repos', {method: "GET"})
+      .then(data => {
+        setResult(data);
+        setIsLoading(false);
+      })
+      .catch(error => console.log(error));
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Machine Learning Projects</p>
+      {!isLoading?
+      <div>
+        <TableContainer component={Paper}>
+          <Table aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell>Description</TableCell>
+                <TableCell>Link</TableCell>
+                <TableCell>Language</TableCell>
+                <TableCell>Created</TableCell>
+                <TableCell>Updated</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {result.map((row) => (
+                <TableRow key={row.full_name}>
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell align="right">{row.description}</TableCell>
+                  <TableCell align="right">{row.link}</TableCell>
+                  <TableCell align="right">{row.language}</TableCell>
+                  <TableCell align="right">{row.created_at}</TableCell>
+                  <TableCell align="right">{row.updated_at}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+      :null}
     </div>
   );
 }
